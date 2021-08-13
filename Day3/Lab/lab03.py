8c0dfea4b40b1e1afd43f98d66e06d6f73ac091a
Ben Noble <bennoble@bens-mbp.dhcp.wustl.edu>
2021-08-13 12:58:24 -0500

    lab03

---
+++ Day3/Lab/lab03.py
@@ -0,0 +1,46 @@
+## 1. write the following functions
+## 2. write a unittest class to test each of these functions once
+## 3. Run it in this script
+
+## Raising errors is more common when developing ------------------------
+
+## These functions all take a single string as an argument.
+## Presumably your code won't work for an int
+## raise a built-in (or custom!) exception if fed an int
+
+
+## make all characters capitalized
+def shout(txt):
+
+
+## reverse all characters in string
+def reverse(txt):
+
+
+## reverse word order in string
+def reversewords(txt):
+
+
+## reverses letters in each word
+def reversewordletters(txt):
+
+
+
+## Try/catch is more common when using
+## someone else's code, scraping, etc. -----------------------------------
+
+## Loop over this string and apply the reverse() function.
+## Should throw errors if your exceptions are being raised!
+## Write a try/catch to handle this.
+ 
+string_list = ["hi", "hello there", 5, "hope this works", 100, "will it?"]
+
+
+		
+			
+			
+			
+			
+			
+			
+
